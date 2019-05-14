0076ea2abec89c9419005da8c003464dfd1d186c
mungmoong <mungmoong@naver.com>
2019-05-14 10:04:40 +0000

    test

---
+++ global-vars/outputs.tf
@@ -0,0 +1,8 @@
+output "bucket_name" {
+  value = "helloworld-terraform-state-test"
+}
+
+output "dynamodb_table" {
+  value = "helloworld-tf-state-lock-test"
+}
+
---
+++ outputs.tf
@@ -0,0 +1,12 @@
+module "global-vars" {
+  source                  = "../global-vars"
+}
+
+output "bucket_name" {
+  value = "${module.global-vars.bucket_name}"
+}
+
+output "dynamodb_table" {
+  value = "${module.global-vars.dynamodb_table}"
+}
+
